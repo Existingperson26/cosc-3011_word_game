87948c28b04ea602784b8733a838f89f819dc2a7
DeaconSteiner <deaconsteiner.it@gmail.com>
2026-02-02 10:25:43 -0700

    Finish Feedback implementation

--- src/wordgame/Feedback.java
+++ src/wordgame/Feedback.java
@@ -1,13 +1,21 @@
 package wordgame;
 
 class Feedback {
+
     private int correctLetters;
     private int correctPositions;
 
-    Feedback() {}
+    Feedback(int correctLetters, int correctPositions) {
+        this.correctLetters = correctLetters;
+        this.correctPositions = correctPositions;
+    }
 
     @Override
-    public String toString() {}
+    public String toString() {
+        return "(" + correctLetters + ", " + correctPositions + ")";
+    }
 
-    public boolean isCorrect(Word word) {}
+    public boolean isCorrect(Word word) {
+        return correctPositions == word.length();
+    }
 }
