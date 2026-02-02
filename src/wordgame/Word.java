e59e58ec97b317dd6c8cb90c2aa3ea5c3f943d30
DeaconSteiner <deaconsteiner.it@gmail.com>
2026-02-02 16:37:58 -0700

    Add constructor definition on Word class

--- src/wordgame/Word.java
+++ src/wordgame/Word.java
@@ -6,7 +6,13 @@ class Word {
 
     private ArrayList<Character> letters;
 
-    Word() {}
+    Word(String str) {
+        letters = new ArrayList<>();
+
+        for (char c : str.toLowerCase().toCharArray()) {
+            letters.add(c);
+        }
+    }
 
     public Feedback compareTo(Word other) {}
 
